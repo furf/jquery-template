a68828c84fab59dee6623956caa79a39299298c9
David Furfero <furf@furfBook-Pro.local>
2009-12-10 01:41:44 -0500

    optimized a bit

--- jquery.template.js
+++ jquery.template.js
@@ -1,17 +1,20 @@
+jQuery.template = function(str, obj) {
+  var replace = 'replace', split = 'split', join = 'join',
+      tmpl = new Function ("__", "__.push('" +
+        str[replace](/[\r\t\n]/g, " ")
+           [split]("<%")[join]("\t")
+           [replace](/((^|%>)[^\t]*)'/g, "$1\r")
+           [replace](/\t=(.*?)%>/g, "',$1,'")
+           [split]("\t")[join]("');")
+           [split]("%>")[join](";__.push('")
+           [split]("\r")[join]("\\'")
+           + "');return __.join('');");
+  function render (obj) {
+    return jQuery(tmpl.call(obj, []));
+  }
+  return obj ? render(obj) : render;
+};
+
 jQuery.fn.template = function(obj) {
-  var fn = (function (str) {
-    var tmpl = new Function ("__", "__.push('" +
-      str.replace(/[\r\t\n]/g, " ")
-         .split("<%").join("\t")
-         .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-         .replace(/\t=(.*?)%>/g, "',$1,'")
-         .split("\t").join("');")
-         .split("%>").join(";__.push('")
-         .split("\r").join("\\'")
-         + "');return __.join('');");
-    return function (obj) {
-      return jQuery(tmpl.call(obj, []));
-    };
-  })(this.html() || '');
-  return obj ? fn(obj) : fn;
+  return $.template(this.html() || '', obj);
 };
