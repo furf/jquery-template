05ec4242ad689c4a6758c0bd5360e5d248b3019b
David Furfero <furf@furfBook-Pro.local>
2010-03-18 03:40:45 -0400

    added caching

--- jquery.template-ajax.js
+++ jquery.template-ajax.js
@@ -6,151 +6,157 @@
  * http://ejohn.org/blog/javascript-micro-templating/
  * MIT Licensed
  */
-jQuery.template = function(cache, str, obj, raw) {
+(function (document, $) {
 
-  var replace = 'replace', split = 'split', join = 'join', source, render, proxy;
+  var cache = {},
 
-  if (typeof cache !== 'boolean') {
-    raw   = obj;
-    obj   = str;
-    str   = cache;
-    cache = true;
-  }
+      // Splits leading text fragment, DOM elements, trailing text fragments
+      quickExpr = /^([^<]*)(<[\w\W]+>)([^>]*)$/,
 
-  /**
-   * Use cached template
-   */
-  if (jQuery.template.cache[str]) {
+      // Tests for Ajax templates
+      httpExpr = /^http\S+$/;
 
-    proxy = jQuery.template.cache[str];
 
-  /**
-   * Load Ajax template
-   */
-  } else if (jQuery.template.httpExpr.test(str)) {
-    
-    jQuery.ajax({
-      url:      str,
-      dataType: 'text',
-      async:    false,
-      success: function (response) {
+  $.template = function(cache, str, obj, raw) {
 
-        proxy = jQuery.template(false, response);
+    var replace = 'replace', split = 'split', join = 'join', source, render, proxy;
 
-        if (cache) {
-          jQuery.template.cache[str] = proxy;
-        }
-      }
-    });
-
-  /**
-   * Create new template
-   */
-  } else {
-
-    // Convert template to JavaScript source code
-    source = "var __=[];__.push('" +
-      str[replace](/[\r\t\n]/g, " ")
-         [split]("{%")[join]("\t")
-         [replace](/((^|%\})[^\t]*)'/g, "$1\r")
-         [replace](/\t=(.*?)%\}/g, "',$1,'")
-         [split]("\t")[join]("');")
-         [split]("%}")[join](";__.push('")
-         [split]("\r")[join]("\\'") +
-         "');return __.join('');";
-
-    // Create the render function from generated source
-    render = new Function(source);
+    if (typeof cache !== 'boolean') {
+      raw = obj;
+      obj = str;
+      str = cache;
+      cache = true;
+    }
 
     /**
-     * Using a proxy function helps us avoid use of the "with" keyword as in the
-     * original micro-templating code. This provides a noticeable performance 
-     * improvement (in most browsers), but requires the use of "this" keyword in
-     * the templates.
+     * Use cached template
      */
-    proxy = function (obj, raw) {
+    if (cache[str]) {
 
-      var html = render.call(obj), match, ret;
+      proxy = cache[str];
 
-      // Return rendered HTML as a string
-      if (raw) {
-        return html;
-      }
+    /**
+     * Load Ajax template
+     */
+    } else if (httpExpr.test(str)) {
 
-      // Return a jQuery object from the rendered HTML
+      $.ajax({
+        url:      str,
+        dataType: 'text',
+        async:    false,
+        success: function (response) {
+
+          proxy = $.template(false, response);
+
+          if (cache) {
+            cache[str] = proxy;
+          }
+        }
+      });
+
+    /**
+     * Create new template
+     */
+    } else {
+
+      // Convert template to JavaScript source code
+      source = "var __=[];__.push('" +
+        str[replace](/[\r\t\n]/g, " ")
+           [split]("{%")[join]("\t")
+           [replace](/((^|%\})[^\t]*)'/g, "$1\r")
+           [replace](/\t=(.*?)%\}/g, "',$1,'")
+           [split]("\t")[join]("');")
+           [split]("%}")[join](";__.push('")
+           [split]("\r")[join]("\\'") +
+           "');return __.join('');";
+
+      // Create the render function from generated source
+      render = new Function(source);
 
       /**
-       * The following block works around a jQuery limitation (as of 1.4.2)
-       * where leading/trailing text fragments are stripped from the selector
-       * when jQuery() is used to create DOM elements.
-       *
-       * $("this <em>doesn't work</em> as expected"); // <em>doesn't work</em>
-       *
-       * A ticket has been filed:
-       * http://dev.jquery.com/ticket/6303
-       *
-       * And a patch has been made:
-       * http://github.com/furf/jquery/blob/master/src/core.js
+       * Using a proxy function helps us avoid use of the "with" keyword as in the
+       * original micro-templating code. This provides a noticeable performance 
+       * improvement (in most browsers), but requires the use of "this" keyword in
+       * the templates.
        */
-      match = jQuery.template.quickExpr.exec(html);
+      proxy = function (obj, raw) {
 
-      if (match) {
+        var html = render.call(obj), match, ret;
 
-        // Create instance from DOM elements
-        ret = jQuery(match[2]);
+        // Return rendered HTML as a string
+        if (raw) {
+          return html;
+        }
 
-        // Prepend leading text
-        Array.prototype.unshift.call(ret, document.createTextNode(match[1]));
-
-        // Append trailing text
-        ret.push(document.createTextNode(match[3]));
-
-      } else {
+        // Return a jQuery object from the rendered HTML
 
         /**
-         * If the rendered HTML contains no DOM elements, use a text node so
-         * jQuery doesn't mistake it for a selector.
+         * The following block works around a jQuery limitation (as of 1.4.2)
+         * where leading/trailing text fragments are stripped from the selector
+         * when jQuery() is used to create DOM elements.
+         *
+         * $("this <em>doesn't work</em> as expected"); // <em>doesn't work</em>
+         *
+         * A ticket has been filed:
+         * http://dev.jquery.com/ticket/6303
+         *
+         * And a patch has been made:
+         * http://github.com/furf/jquery/blob/master/src/core.js
          */
-        ret = jQuery(document.createTextNode(html));
+        match = quickExpr.exec(html);
+
+        if (match) {
+
+          // Create instance from DOM elements
+          ret = $(match[2]);
+
+          // Prepend leading text
+          Array.prototype.unshift.call(ret, document.createTextNode(match[1]));
+
+          // Append trailing text
+          ret.push(document.createTextNode(match[3]));
+
+        } else {
+
+          /**
+           * If the rendered HTML contains no DOM elements, use a text node so
+           * jQuery doesn't mistake it for a selector.
+           */
+          ret = $(document.createTextNode(html));
+        }
+
+        return ret;
+      };
+
+      /**
+       * Aliased toString and toSource methods allow access to rendered template
+       * function source code for improved debugging.
+       */
+      proxy.toString = function () {
+        return render.toString();
+      };
+
+      if (render.toSource) {
+        proxy.toSource = function () {
+          return render.toSource();
+        };
       }
 
-      return ret;
-    };
-
-    /**
-     * Aliased toString and toSource methods allow access to rendered template
-     * function source code for improved debugging.
-     */
-    proxy.toString = function () {
-      return render.toString();
-    };
-
-    if (render.toSource) {
-      proxy.toSource = function () {
-        return render.toSource();
-      };
+      // Cache proxy for faster retrieval on subsequent calls
+      if (cache) {
+        cache[str] = proxy;
+      }
     }
 
-    if (cache) {
-      jQuery.template.cache[str] = proxy;
-    }
-  }
+    // Return template engine or rendered HTML if an object is specified
+    return obj ? proxy(obj, raw) : proxy;
+  };
 
-  // Return template engine or rendered HTML if an object is specified
-  return obj ? proxy(obj, raw) : proxy;
-};
+  /**
+   * Parse selected elements as templates
+   */
+  $.fn.template = function(obj, raw) {
+    return $.template(this.text() || '', obj, raw);
+  };
 
-jQuery.template.cache = {};
-
-// Splits leading text fragment, DOM elements, trailing text fragments
-jQuery.template.quickExpr = /^([^<]*)(<[\w\W]+>)([^>]*)$/;
-
-// Tests for Ajax templates
-jQuery.template.httpExpr = /^http\S+$/;
-
-/**
- * Parse selected elements as templates
- */
-jQuery.fn.template = function(obj, raw) {
-  return jQuery.template(this.text() || '', obj, raw);
-};
+})(document, jQuery);
