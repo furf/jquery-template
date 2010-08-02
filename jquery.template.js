ad15f15e1d155970a7286d56afc3a2c73539103a
Dave Furfero <furftastic@yahoo.com>
2010-08-02 14:43:02 -0400

    consolidated

--- jquery.template.js
+++ jquery.template.js
@@ -6,105 +6,148 @@
  * http://ejohn.org/blog/javascript-micro-templating/
  * MIT Licensed
  */
-jQuery.template = function(str, obj, raw) {
+(function (document, $) {
 
-  var replace = 'replace', split = 'split', join = 'join',
+  var templates = {},     // Cache for previously rendered templates
+      tokenExpr = /\{%/;  // Rough test for non-templates, ie. URLs
+
+  /**
+   * Creates a template engine based on a specified template, either a string
+   * or URL to template file.
+   * @param Boolean cache (optional) Set false to avoid cache, default true 
+   * @param String  str Parseable HTML template (must contain at least one
+   *                tag, {% ... %}) or URL to template file
+   * @param Object  obj (optional) data object to render
+   * @param Boolean raw (optional) set true to render as pure HTML, set false to
+   *                render as jQuery collection, default false
+   * @return Object jQuery collection containing one member, <jquery:template/>,
+   *                which will contain contents of rendered template
+   */
+  $.template = function (cache, str, obj, raw) {
+
+    var replace = 'replace',
+        split   = 'split',
+        join    = 'join',
+        tag     = 'jquery:template',
+        source,
+        render,
+        proxy;
+
+    // If optional first argument is not supplied, shift arguments
+    if (typeof cache !== 'boolean') {
+      raw   = obj;
+      obj   = str;
+      str   = cache;
+      cache = true;
+    }
+
+    /**
+     * If str matches a key in the cache, use the previously rendered template
+     */
+    if (str in templates) {
+
+      proxy = templates[str];
+
+    /**
+     * If str does not contain any template tags, async load the Ajax template
+     */
+    } else if (!tokenExpr.test(str)) {
+
+      $.ajax({
+        url:      str,
+        dataType: 'text',
+        async:    false,
+        success: function (response) {
+
+          proxy = $.template(false, response);
+
+          if (cache) {
+            templates[str] = proxy;
+          }
+        }
+      });
+
+    /**
+     * Parse str as JavaScript source to create new template engine
+     */
+    } else {
 
       // Convert template to JavaScript source code
       source = "var __=[];__.push('" +
         str[replace](/[\r\t\n]/g, " ")
            [split]("{%")[join]("\t")
            [replace](/((^|%\})[^\t]*)'/g, "$1\r")
+
+           /**
+            * NOTE! Due to a bug in the micro-template parser, where templates
+            * containing more than one single quote (') fail to render, we
+            * escape all single quotes. The downside is that you must use
+            * double quotes (") to encapsulate strings in your code blocks.
+            * Example: {% var foo = "bar"; }
+            */
+           [split]("'")[join]("\\'")
+           
            [replace](/\t=(.*?)%\}/g, "',$1,'")
            [split]("\t")[join]("');")
            [split]("%}")[join](";__.push('")
            [split]("\r")[join]("\\'") +
-           "');return __.join('');",
-      
+           "');return __.join('');";
+
       // Create the render function from generated source
       render = new Function(source);
 
-  /**
-   * Using a proxy function helps us avoid use of the "with" keyword as in the
-   * original micro-templating code. This provides a noticeable performance 
-   * improvement (in most browsers), but requires the use of "this" keyword in
-   * the templates.
-   */
-  function proxy (obj, raw) {
-    
-    var html = render.call(obj), match, ret;
-        
-    // Return rendered HTML as a string
-    if (raw) {
-      return html;
-    }
+      /**
+       * Using a proxy function helps us avoid use of the "with" keyword, used
+       * in the original micro-templating code. This provides a noticeable
+       * performance improvement (in most browsers), but requires the use of
+       * "this" keyword in the templates.
+       * Example:
+       * $('myTemplate.tpl', {name:'furf'});
+       * myTemplate.tpl:
+       * Hello, <em>{%= this.name %}</em>
+       */
+      proxy = function (obj, raw) {
 
-    // Return a jQuery object from the rendered HTML
+        // If object is an Array, render its members , otherwise render object
+        var html = $.isArray(obj) ? $.map(obj, function (obj) {
+          return render.call(obj);
+        }).join('') : render.call(obj);
 
-    /**
-     * The following block works around a jQuery limitation (as of 1.4.2)
-     * where leading/trailing text fragments are stripped from the selector
-     * when jQuery() is used to create DOM elements.
-     *
-     * $("this <em>doesn't work</em> as expected"); // <em>doesn't work</em>
-     *
-     * A ticket has been filed:
-     * http://dev.jquery.com/ticket/6303
-     *
-     * And a patch has been made:
-     * http://github.com/furf/jquery/blob/master/src/core.js
-     */
-    match = jQuery.template.quickExpr.exec(html);
-    
-    if (match) {
-
-      // Create instance from DOM elements
-      ret = jQuery(match[2]);
-
-      // Prepend leading text
-      Array.prototype.unshift.call(ret, document.createTextNode(match[1]));
-
-      // Append trailing text
-      ret.push(document.createTextNode(match[3]));
-
-    } else {
+        // Return rendered HTML as a string or as jQuery instance
+        return raw ? html : $('<' + tag + '>' + html + '</' + tag + '>');
+      };
 
       /**
-       * If the rendered HTML contains no DOM elements, use a text node so
-       * jQuery doesn't mistake it for a selector.
+       * Aliased toString and toSource methods allow access to rendered template
+       * function source code for improved debugging.
        */
-      ret = jQuery(document.createTextNode(html));
+      proxy.toString = function () {
+        return render.toString();
+      };
+
+      if (render.toSource) {
+        proxy.toSource = function () {
+          return render.toSource();
+        };
+      }
+
+      // Cache proxy for faster retrieval on subsequent calls
+      if (cache) {
+        templates[str] = proxy;
+      }
     }
 
-    return ret;
-  }
+    // Return template engine or rendered HTML if an object is specified
+    return obj ? proxy(obj, raw) : proxy;
+  };
 
   /**
-   * Aliased toString and toSource methods allow access to rendered template
-   * function source code for improved debugging.
+   * Parse selected elements as templates
+   * @todo make this function consistent among dome elements
    */
-  proxy.toString = function () {
-    return render.toString();
+  $.fn.template = function (obj, raw) {
+    console.dir(this[0]);
+    return $.template(this.text() || '', obj, raw);
   };
-  
-  if (render.toSource) {
-    proxy.toSource = function () {
-      return render.toSource();
-    };
-  }
-  
-  // Return template engine or rendered HTML if an object is specified
-  return obj ? proxy(obj, raw) : proxy;
-};
 
-
-// Splits leading text fragment, DOM elements, trailing text fragments
-jQuery.template.quickExpr = /^([^<]*)(<[\w\W]+>)([^>]*)$/;
-
-
-/**
- * Parse selected elements as templates
- */
-jQuery.fn.template = function(obj, raw) {
-  return jQuery.template(this.text() || '', obj, raw);
-};
+})(this.document, this.jQuery);
